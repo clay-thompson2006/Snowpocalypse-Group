225810b620a846ff8e27fb1ad134a2599c96f7e9
clay-thompson2006 <143003832+clay-thompson2006@users.noreply.github.com>
2023-09-18 07:54:31 -0400

    Add files via upload

---
+++ SMS.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+
+account_sid = "AC823b7b5840f23f6cf0c953566d304dc9"
+auth_token = "c5b77ab69eb741e0228f0de412f59d63"
+client = Client(account_sid, auth_token)
+
+sender_phone_number = "+18559663985"  # Your Twilio phone number
+recipient_phone_number = "+1 347 966 4652"  # Recipient's phone number
+
+message = client.messages.create(
+    body="Mr. Sekol This is your alarm to tell you that there might be a snow day so have fun with the puppers and have a great day.",
+    from_=sender_phone_number,
+    to=recipient_phone_number
+)
+
+print("SMS sent with SID:", message.sid)
