72cdc9c5042b9896d8f6f49eb959c12ab93b1331
clay-thompson2006 <143003832+clay-thompson2006@users.noreply.github.com>
2023-09-18 08:49:32 -0400

    commit

--- Gmail.py
+++ Gmail.py
@@ -2,8 +2,9 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 #Gmail Parameters
-sender_email = "Snowpocalypse.Notifications@gmail.com"
-recipient_email = "michael.sekol@mahoningctc.com"
+sender_email = "snowpocalypse.notifications@gmail.com"
+recipient_email = "phillips_clayton@student.mahoningctc.com"
+#recipient_email = "michael.sekol@mahoningctc.com"
 subject = "Snow Day Alert"
 message = "Mr. Sekol This is your alarm to tell you that there might be a snow day so have fun with the puppers and have a great day."
 
@@ -21,7 +22,7 @@ smtp_port = 587  # 587 is the TLS/STARTTLS port for Gmail
 server = smtplib.SMTP(smtp_server, smtp_port)
 server.starttls()  # Enable TLS encryption
 
-email_password = "MCCTC2023-2024"
+email_password = "mcctc2023-2024"
 server.login(sender_email, email_password)
 
 server.sendmail(sender_email, recipient_email, msg.as_string())
