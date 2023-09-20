4ffc22fd1bda54234a6037afa4ad47259373f660
clay-thompson2006 <143003832+clay-thompson2006@users.noreply.github.com>
2023-09-20 09:10:21 -0400

    commit

--- SMS.py
+++ SMS.py
@@ -5,7 +5,7 @@ auth_token = "c5b77ab69eb741e0228f0de412f59d63"
 client = Client(account_sid, auth_token)
 
 sender_phone_number = "+18559663985"  # Your Twilio phone number
-recipient_phone_number = "+1 347 966 4652"  # Recipient's phone number
+recipient_phone_number = "+1 330 446 0711"  # Recipient's phone number
 
 message = client.messages.create(
     body="Mr. Sekol This is your alarm to tell you that there might be a snow day so have fun with the puppers and have a great day.",
